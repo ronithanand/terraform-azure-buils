33ec23bc6258ff11a475da16752e7ac594bf6d74
ronithanand <38489297+ronithanand@users.noreply.github.com>
2020-10-07 14:08:12 +0000

    Update vm.tf

--- virtualmachinewinazure/vm.tf
+++ virtualmachinewinazure/vm.tf
@@ -8,7 +8,7 @@ resource "azurerm_windows_virtual_machine" "windos2016" {
   admin_password      = "P@$$w0rd1234!"
   availability_set_id = azurerm_availability_set.win-2016.id
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.niccardid.id,
   ]
 
   os_disk {
